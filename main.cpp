083bb3cf24b5fd8e92bdbe5ce5588e69fa76786c
jaiveerbhaidasna <43055551+jaiveerbhaidasna@users.noreply.github.com>
2019-11-18 14:35:11 -0600

    BST complete
    All functions written, just need to test

---
+++ main.cpp
@@ -0,0 +1,78 @@
+// Sample driver
+// EE 312 Fall 2018
+//
+// The BST should work for any data type or object.
+// Other user created objects must have the << operator overloaded for 
+// output.
+//
+
+#include <iostream>
+#include <fstream>
+#include "BST312.h"
+
+using namespace std;
+
+template <typename T>
+void print(vector<T> vec){
+    for(int i = 0 ; i < vec.size() ; i++){
+        cout << vec[i] << endl;
+    }
+    cout << "\n";
+}
+
+int main() {
+    /*
+    ifstream inFile;
+    inFile.open("test.txt");
+
+    BST_312<string> bst;
+   
+    string s;
+    inFile >> s;
+    while (inFile) {;
+        cout << "inserting ... " << s << endl;
+        bst.insertItem(s);
+        inFile >> s;
+    }
+
+    cout << "postorder traversal is " << endl;
+    print(bst.postOrderTraversal());
+    cout << endl;
+
+    cout << "preorder traversal is " << endl;
+    print(bst.preOrderTraversal());
+    cout << endl;
+
+    cout << "inorder traversal is " << endl;
+    print(bst.inOrderTraversal());
+    cout << endl;
+
+    cout << "Remove items " << endl;
+    cout << "number of nodes in tree before delete is " << bst.countNodes() << endl;
+    s = "tree";
+    bst.deleteItem(s);
+    print(bst.postOrderTraversal());
+    cout << endl;
+    cout << "number of nodes in tree after delete is " << bst.countNodes() << endl;
+    cout << endl;
+    */
+
+    BST_312<int> bst;
+    bst.insertItem(10);
+    bst.insertItem(11);
+    bst.insertItem(7);
+    bst.insertItem(19);
+    bst.insertItem(13);
+    bst.insertItem(6);
+    bst.insertItem(3);
+    bst.insertItem(8);
+
+    print(bst.preOrderTraversal());
+    print(bst.inOrderTraversal());
+    print(bst.postOrderTraversal());
+
+    cout << bst.isItemInTree(20) << "\n";
+
+
+}
+
